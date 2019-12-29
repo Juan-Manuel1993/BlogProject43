b0f54cbd697319df41f7fac78fb981926b90b65b
maiddima <imrhan-dareine.minko-amoa@etu.umontpellier.fr>
2019-12-29 18:33:30 +0100

    add article form change

--- src/Controller/blogcontroller.php
+++ src/Controller/blogcontroller.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Entity\User;
 use App\Entity\Tag;
 use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -33,9 +34,52 @@ class blogcontroller extends AbstractController
     /**
     * @Route("/add")
     */
-    public function article_add(){
+    public function article_add(Request $request){
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setUpdatedAt(new \DateTime());
+
+            $article->setCreatedAt(new \DateTime());
+
+            $em = $this->getDoctrine()->getManager(); // On récupère l'entity manager
+            $em->persist($article); // On confie notre entité à l'entity manager (on persist l'entité)
+            $em->flush(); // On execute la requete
+
+            return new Response('L\'article a bien été enregistrer.');
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setLastUpdateDate(new \DateTime());
+
+            if ($article->getPicture() !== null) {
+                $file = $form->get('picture')->getData();
+                $fileName =  uniqid(). '.' .$file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('post_images_directory'), // Le dossier dans le quel le fichier va etre charger
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $article->setPicture($fileName);
+            }
+
+            if ($article->getIsPublished()) {
+                $article->setPublicationDate(new \DateTime());
+            }
+
+            $em = $this->getDoctrine()->getManager(); // On récupère l'entity manager
+            $em->persist($article); // On confie notre entité à l'entity manager (on persist l'entité)
+            $em->flush(); // On execute la requete
+
+            return new Response('L\'article a bien été enregistrer.');
+        }
 
     	return $this->render('add.html.twig', [
             'form' => $form->createView()
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -19,6 +20,7 @@ class Article
     private $id;
 
     /**
+     * @Assert\NotBlank(message="Ce champ ne peut pas etre vide.")
      * @ORM\Column(type="string", length=255)
      */
     private $title;
