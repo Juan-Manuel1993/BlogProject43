85d715acec03d9c97c1c929eac84c621f733856f
Zoro-Bi <zorobizah@yahoo.fr>
2020-01-02 14:29:49 +0100

    Peut ajouter une photo de profil

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,7 +4,8 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
-
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
@@ -24,8 +25,32 @@ class User extends BaseUser
 
     }
 
-    
+      /**
+     * @ORM\Column(type="string",nullable=true)
+     */
+    public $image;
 
+    protected function getimage_user(): ?string
+    {
+        return $this->image;
+    }
+
+    protected function setimage_user($image_user)
+    {   
+        $this->image = $image;
+
+        return $this;
+    }
+
+      public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('image', new Assert\Image([
+            'minWidth' => 200,
+            'maxWidth' => 400,
+            'minHeight' => 200,
+            'maxHeight' => 400,
+        ]));
+    }
 
     public function GetId(): ?int
     {
